vibs29/bordeaux-threads

commit c272d5a4cc1c4f54fcd21dbd6a29c3d23da4d5e4

Add (RECURSIVE-)LOCK types and respective predicates

src/impl-clisp.lisp
@@ -28,6 +28,18 @@ Distributed under the MIT license (see LICENSE file)
 
 ;;; Resource contention: locks and recursive locks
 
+(deftype lock () 'mt:mt-mutex)
+
+(deftype recursive-lock ()
+  '(and mt:mt-mutex (satisfies mt:mutex-recursive-p)))
+
+(defun lock-p (object)
+  (typep object 'mt:mt-mutex))
+
+(defun recursive-lock-p (object)
+  (and (typep object 'mt:mt-mutex)
+       (mt:mutex-recursive-p object)))
+
 (defun make-lock (&optional name)
   (mt:make-mutex :name (or name "Anonymous lock")))
 

src/default-implementations.lisp
@@ -80,6 +80,16 @@ It is safe to call repeatedly."
 
 ;;; Resource contention: locks and recursive locks
 
+(defdfun lock-p (object)
+  "Returns T if OBJECT is a lock; returns NIL otherwise."
+  (declare (ignore object))
+  (values))
+
+(defdfun recursive-lock-p (object)
+  "Returns T if OBJECT is a recursive lock; returns NIL otherwise."
+  (declare (ignore object))
+  (values))
+
 (defdfun make-lock (&optional name)
   "Creates a lock (a mutex) whose name is NAME. If the system does not
   support multiple threads this will still return some object, but it

src/impl-abcl.lisp
@@ -48,6 +48,16 @@ Distributed under the MIT license (see LICENSE file)
 (defconstant +get-hold-count+ 
   (jmethod "java.util.concurrent.locks.ReentrantLock" "getHoldCount"))
 
+(deftype lock () 'mutex)
+
+(deftype recursive-lock () 'mutex-recursive)
+
+(defun lock-p (object)
+  (typep object 'mutex))
+
+(defun recursive-lock-p (object)
+  (typep object 'mutex-recursive))
+
 (defun make-lock (&optional name)
   (make-mutex 
    :name (or name "Anonymous lock")

src/impl-allegro.lisp
@@ -13,6 +13,16 @@ Distributed under the MIT license (see LICENSE file)
 
 ;;; Resource contention: locks and recursive locks
 
+(deftype lock () 'mp:process-lock)
+
+(deftype recursive-lock () 'mp:process-lock)
+
+(defun lock-p (object)
+  (typep object 'mp:process-lock))
+
+(defun recursive-lock-p (object)
+  (typep object 'mp:process-lock))
+
 (defun make-lock (&optional name)
   (mp:make-process-lock :name (or name "Anonymous lock")))
 

src/impl-clozure.lisp
@@ -30,6 +30,16 @@ Distributed under the MIT license (see LICENSE file)
 
 ;;; Resource contention: locks and recursive locks
 
+(deftype lock () 'ccl:lock)
+
+(deftype recursive-lock () 'ccl:lock)
+
+(defun lock-p (object)
+  (typep object 'ccl:lock))
+
+(defun recursive-lock-p (object)
+  (typep object 'ccl:lock))
+
 (defun make-lock (&optional name)
   (ccl:make-lock (or name "Anonymous lock")))
 

src/impl-cmucl.lisp
@@ -40,6 +40,16 @@ Distributed under the MIT license (see LICENSE file)
 
 ;;; Resource contention: locks and recursive locks
 
+(deftype lock () 'mp:error-check-lock)
+
+(deftype recursive-lock () 'mp:recursive-lock)
+
+(defun lock-p (object)
+  (typep object 'mp:error-check-lock))
+
+(defun recursive-lock-p (object)
+  (typep object 'mp:recursive-lock))
+
 (defun make-lock (&optional name)
   (mp:make-lock (or name "Anonymous lock")
                 :kind :error-check))

src/impl-ecl.lisp
@@ -30,6 +30,18 @@ Distributed under the MIT license (see LICENSE file)
 
 ;;; Resource contention: locks and recursive locks
 
+(deftype lock () 'mp:lock)
+
+(deftype recursive-lock ()
+  '(and mp:lock (satisfies mp:recursive-lock-p)))
+
+(defun lock-p (object)
+  (typep object 'mp:lock))
+
+(defun recursive-lock-p (object)
+  (and (typep object 'mp:lock)
+       (mp:recursive-lock-p object)))
+
 (defun make-lock (&optional name)
   (mp:make-lock :name (or name "Anonymous lock")))
 

src/impl-lispworks.lisp
@@ -44,6 +44,19 @@ Distributed under the MIT license (see LICENSE file)
 
 ;;; Resource contention: locks and recursive locks
 
+
+(deftype lock () 'mp:lock)
+
+(deftype recursive-lock ()
+  '(and mp:lock (satisfies mp:lock-recursive-p)))
+
+(defun lock-p (object)
+  (typep object 'mp:lock))
+
+(defun recursive-lock-p (object)
+  (and (typep object 'mp:lock)
+       (mp:lock-recursive-p object)))
+
 (defun make-lock (&optional name)
   (mp:make-lock :name (or name "Anonymous lock")
                 #-(or lispworks4 lispworks5) :recursivep

commit e2e4e69007e5c1a39dff989101079a6bb0b00e9a

fix clisp mt-mutex compile error introduced in commit c272d5a4cc1c4f54fcd21dbd6a29c3d23da4d5e4

src/impl-clisp.lisp
@@ -28,16 +28,16 @@ Distributed under the MIT license (see LICENSE file)
 
 ;;; Resource contention: locks and recursive locks
 
-(deftype lock () 'mt:mt-mutex)
+(deftype lock () 'mt:mutex)
 
 (deftype recursive-lock ()
-  '(and mt:mt-mutex (satisfies mt:mutex-recursive-p)))
+  '(and mt:mutex (satisfies mt:mutex-recursive-p)))
 
 (defun lock-p (object)
-  (typep object 'mt:mt-mutex))
+  (typep object 'mt:mutex))
 
 (defun recursive-lock-p (object)
-  (and (typep object 'mt:mt-mutex)
+  (and (typep object 'mt:mutex)
        (mt:mutex-recursive-p object)))
 
 (defun make-lock (&optional name)